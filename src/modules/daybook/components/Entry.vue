52da558d4bbfbae2bc1d51621acb787171b5bcc6
IKLAB <hernan@infokrause.com>
2021-11-06 20:26:45 -0300

    Primer commit

---
+++ src/modules/daybook/components/Entry.vue
@@ -0,0 +1,31 @@
+<template>
+    <div 
+        class="entry-container mb-3 pointer p-2"
+        @click="$router.push({ name: 'entry', params: { id: 10} })"
+    >
+        <div class="entry-title d-flex">
+            <span class="text-success fs-5 fw-bold">05</span>
+            <span class="mx-1 fs-5">Noviembre</span>
+            <span class="mx-2 fw-light">2021, Viernes</span>
+        </div>
+
+        <div class="entry-description">
+            Lorem nostrud magna ullamco eiusmod ex commodo magna sit nulla do occaecat laborum. Sunt magna proident irure magna dolore laborum qui irure commodo voluptate pariatur excepteur. Elit cillum voluptate Lorem occaecat cillum labore nostrud.
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.entry-container {
+    border-bottom: 1px solid #2c3e50;
+    transition: 0.2s all ease-in;
+    &:hover {
+        background-color: lighten($color: grey, $amount: 45);
+        transition: 0.2s all ease-in;
+    };
+
+    .entry-description {
+        font-size: 12px;
+    }
+}
+</style>
---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,24 @@
+
+export default () => ({
+    isLoading: true,
+    entries: [
+        {
+            id: new Date().getTime(),
+            date: new Date().toDateString(),
+            text: 'Nostrud dolor Lorem ad nisi Lorem proident qui ad ad fugiat commodo et exercitation. Sunt laborum non ex ullamco eiusmod quis esse eiusmod. Ex proident dolor exercitation in laborum in dolore dolore. Amet deserunt quis irure sunt veniam duis id laborum.',
+            picture: null
+        },
+        {
+            id: new Date().getTime() + 1000,
+            date: new Date().toDateString(),
+            text: 'Mollit consectetur enim cillum in. Incididunt adipisicing fugiat et amet enim consectetur et irure. Mollit voluptate ipsum enim officia pariatur est eu non ad consequat cillum sunt. Tempor laboris dolore Lorem nisi fugiat sint est culpa dolor consequat exercitation proident consectetur pariatur. Lorem veniam occaecat adipisicing nisi aliqua sint eu. Ullamco cillum aliquip mollit consectetur fugiat nisi. Nulla enim sit nisi aliquip nulla duis.',
+            picture: null
+        },
+        {
+            id: new Date().getTime() + 2000,
+            date: new Date().toDateString(),
+            text: 'Deserunt ipsum proident et excepteur Lorem magna. Eu dolor nostrud adipisicing irure incididunt deserunt sit aliqua nostrud. Voluptate deserunt proident est officia eiusmod nisi amet et minim exercitation incididunt commodo elit. Tempor elit dolor irure duis sunt. Exercitation labore proident aute eiusmod minim consectetur tempor consequat irure reprehenderit tempor do incididunt. Excepteur fugiat ut duis amet in quis commodo id mollit quis ex. Sint sit anim eiusmod in excepteur ipsum est pariatur eu.',
+            picture: null
+        }
+    ]
+})
